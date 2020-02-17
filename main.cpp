1c1875cd23b195c662aca69d31ae09321293dd86
limarevamasha <limareva.miu@phystech.edu>
2020-02-17 21:40:03 +0300

    1 part

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include < stdio.h >
 int main (void)
 {
-puts (“Hello world”);
-puts (“I am in repository”);
-
-return 0;
-}int main (void)
-{
-puts (“Hello world”);
+printf (“Hello world/n”);
 return 0;
 }
