a9e74340c36a8132e3441f4e35ea5562100ee798
limarevamasha <limareva.miu@phystech.edu>
2020-02-17 20:43:05 +0300

    Change

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include < stdio.h > 
+#include < stdio.h >
 int main (void)
+{
+puts (“Hello world”);
+puts (“I am in repository”);
+
+return 0;
+}int main (void)
 {
 puts (“Hello world”);
 return 0;
